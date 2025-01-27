fddae98e5f81a3482ba7861b225e57bf84787ec6
0xhardman <1142783160@qq.com>
2025-01-27 11:39:45 +0800

    fix color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,17 +15,17 @@ export default function Hero() {
       <img src="/bg/blur2.png" className="absolute w-[784px] right-0 top-0" alt="" />
       <img src="/bg/circle.png" className="absolute w-[780px] bottom-[150px] right-0" alt="" />
       <div className="bg-[url('/bg/vector.png')] w-fit bg-[length:140px_140px] bg-no-repeat bg-left-top pt-8 pl-6 relative top-[180px] left-[248px]">
-        <h1 className="text-6xl font-bold mb-6">
+        <h1 className="text-5xl font-bold mb-6 leading-[5rem]">
           Onboarding the next billion<br />
-          <span className="mt-6">
-            <del className="text-4xl">USERS</del>
+          <span className="">
+            <del className="text-4xl text-[#B3B3B3]">USERS</del>
             <span>&nbsp;</span>
-            <span className="text-[#00FF85]">AGENTS.</span>
+            <span className="text-[#00FF85] text-6xl">AGENTS.</span>
           </span>
         </h1>
         <button
           onClick={() => setIsDialogOpen(true)}
-          className=" text-[#14F46F]  border border-[#14F46F] px-4 py-2 rounded-md font-semibold text-xl mt-4 hover:bg-[#14F46F]/10 transition-colors"
+          className=" text-[#14F46F]  border border-[#14F46F] px-6 py-3 rounded-md font-semibold text-xl mt-4 hover:bg-[#14F46F]/10 transition-colors"
         >
           Join Waitlist
         </button>
